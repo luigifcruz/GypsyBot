64fa91abd2db8fa62e45023388eba78bfeff418a
Luigi Freitas Cruz <luigifcruz@icloud.com>
2017-08-26 18:20:28 -0300

    Docker Fixes

--- docker_start.sh
+++ docker_start.sh
@@ -1,5 +1,1 @@
-docker run \
-    --name gypsy-bot \ 
-    --restart=always \
-    -p 8080:20003/tcp: \
-    luigifcruz/gypsybot
+docker run -d --name gypsy-bot --restart=always -p 8080:20003/tcp luigifcruz/gypsybot
